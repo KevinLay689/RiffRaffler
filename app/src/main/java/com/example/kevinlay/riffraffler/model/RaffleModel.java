0f97480ea83423501655419dd6f300e7ca6dad51
Kevin Lay <kevinlay689@yahoo.com>
2017-11-13 16:23:05 -0800

    "Added database work"

---
+++ app/src/main/java/com/example/kevinlay/riffraffler/model/RaffleModel.java
@@ -0,0 +1,34 @@
+package com.example.kevinlay.riffraffler.model;
+
+/**
+ * Created by kevinlay on 11/13/17.
+ */
+
+public class RaffleModel {
+
+    boolean isWinner;
+    boolean isActive;
+    String raffleID;
+
+    public RaffleModel(String raffleID) {
+        isWinner = false;
+        isActive = true;
+        this.raffleID = raffleID;
+    }
+
+    public boolean isWinner() {
+        return isWinner;
+    }
+
+    public void setWinner(boolean winner) {
+        isWinner = winner;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
