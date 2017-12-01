f9db23998cb7f26f197b4ebf9b9cdf8b3e57d5b6
Kevin Lay <kevinlay689@yahoo.com>
2017-11-30 19:28:43 -0800

    -Card view prototype done

--- app/src/main/java/com/example/kevinlay/riffraffler/adapter/MessagesAdapter.java
+++ app/src/main/java/com/example/kevinlay/riffraffler/adapter/MessagesAdapter.java
@@ -4,10 +4,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kevinlay.riffraffler.model.MessagesModel;
 import com.example.kevinlay.riffraffler.R;
+import com.example.kevinlay.riffraffler.model.RaffleTicketModel;
 
 import java.util.List;
 
@@ -18,24 +20,31 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 
 public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.MessagesViewHolder> {
-    private List<MessagesModel> messages;
+    private List<RaffleTicketModel> messages;
 
-    public MessagesAdapter(List<MessagesModel> messageModel) {
+    public MessagesAdapter(List<RaffleTicketModel> messageModel) {
         messages = messageModel;
     }
 
     @Override
     public MessagesAdapter.MessagesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.message_single_item, parent, false);
+                .inflate(R.layout.card_layout, parent, false);
 
-        return new MessagesAdapter.MessagesViewHolder(view);
+        MessagesViewHolder myViewHolder = new MessagesViewHolder(view);
+        return myViewHolder;
     }
 
     @Override
     public void onBindViewHolder(MessagesAdapter.MessagesViewHolder holder, int position) {
-        holder.textView.setText(messages.get(position).getMessageName());
-        //holder.imageView.setImageResource(raffles.get(position).getImage());
+
+        TextView textViewName = holder.textViewName;
+        TextView textViewVersion = holder.textViewVersion;
+        TextView textViewH = holder.textViewH;
+
+        textViewName.setText("Raffle Owner Id: " + messages.get(position).getOwner());
+        textViewVersion.setText(messages.get(position).getRaffleName());
+        textViewH.setText("Raffle ID: " + messages.get(position).getRaffleId());
     }
 
     @Override
@@ -45,13 +54,15 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.Messag
 
     public class MessagesViewHolder extends RecyclerView.ViewHolder {
 
-        private CircleImageView imageView;
-        private TextView textView;
+        TextView textViewName;
+        TextView textViewVersion;
+        TextView textViewH;
 
         public MessagesViewHolder(View itemView) {
             super(itemView);
-            textView = itemView.findViewById(R.id.messagesTextView);
-            imageView = itemView.findViewById(R.id.message_profile_image);
+            this.textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+            this.textViewVersion = (TextView) itemView.findViewById(R.id.textViewVersion);
+            this.textViewH = (TextView) itemView.findViewById(R.id.textViewH);
         }
     }
 }
